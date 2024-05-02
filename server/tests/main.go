0537a9a77ad27127ea0fa96a05f8b0508cb1dbcc
Leonhard Breuer <contact@cheetahbyte.dev>
2024-05-02 16:12:58 +0200

    test(TestHelper): add tests helper

--- server/tests/main.go
+++ server/tests/main.go
@@ -2,8 +2,11 @@ package tests
 
 import (
 	"keinbudget/server/src/database"
+	"keinbudget/server/src/database/models"
+	"keinbudget/server/src/dto"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func SetupRouter() *gin.Engine {
@@ -11,4 +14,19 @@ func SetupRouter() *gin.Engine {
 	app := gin.Default()
 	database.SetupDatabase(":memory:")
 	return app
+}
+
+type TestHelper struct {
+	DB *gorm.DB
+}
+
+func (helper *TestHelper) CreateAccount(data *dto.AccountCreate) models.Account {
+	account := models.Account{
+		Name:            data.Name,
+		Iban:            data.Iban,
+		Balance:         data.Balance,
+		StartingBalance: data.StartingBalance,
+	}
+	helper.DB.Create(&account)
+	return account
 }
