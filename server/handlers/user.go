6e2f7c545535c631d37da58851c2f371d80fe0c9
Leonhard Breuer <me@leobreuer.dev>
2024-07-04 13:45:23 +0200

    feat(backend/security): add encryption for passwords using argon2id

--- server/handlers/user.go
+++ server/handlers/user.go
@@ -1,11 +1,18 @@
 package handlers
 
 import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
+	"golang.org/x/crypto/argon2"
 )
 
 type UserHandler struct {
@@ -41,10 +48,16 @@ func (handler *UserHandler) NewUser(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusUnprocessableEntity).SendString("no password or username specified")
 	}
 
+	hash, err := hashPassword(userData.Password, buildArgon2Params())
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
+	}
+
+	user := User{ID: uuid.New(), Username: userData.Username, Password: string(hash)}
+
 	tx := handler.DB.MustBegin()
 
-	user := User{ID: uuid.New(), Username: userData.Username, Password: userData.Password}
-	_, err := tx.Exec("insert into users (id, username, password) values($1, $2, $3);", user.ID, user.Username, user.Password)
+	_, err = tx.NamedExec("insert into users (id, username, password) values(:id, :username, :password);", &user)
 
 	if err != nil {
 		tx.Rollback()
@@ -73,8 +86,14 @@ func (handler *UserHandler) LoginUser(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusNotFound).SendString(err.Error())
 	}
 
-	if loginData.Password != foundUser.Password {
+	match, err := verifyPassword(loginData.Password, foundUser.Password)
+
+	if !match {
 		return c.Status(fiber.StatusUnauthorized).SendString("passwords not matching")
+	}
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).SendString(err.Error())
 	}
 
 	session := Session{
@@ -100,4 +119,105 @@ func (handler *UserHandler) LoginUser(c *fiber.Ctx) error {
 	c.Cookie(&keks)
 	c.Response().Header.Add("Content-Type", "application/json")
 	return c.SendString("{ok: 1}")
+}
+
+type Argon2Params struct {
+	Memory      uint32
+	Iterations  uint32
+	Parallelism uint8
+	SaltLength  uint32
+	KeyLength   uint32
+}
+
+func buildArgon2Params() *Argon2Params {
+	return &Argon2Params{
+		Memory:      64 * 1024,
+		Iterations:  3,
+		Parallelism: 2,
+		SaltLength:  16,
+		KeyLength:   32,
+	}
+}
+
+func hashPassword(password string, p *Argon2Params) (encodedHash string, err error) {
+	salt, err := generateRandomBytes(p.SaltLength)
+	if err != nil {
+		return "", err
+	}
+
+	hash := argon2.IDKey([]byte(password), salt, p.Iterations, p.Memory, p.Parallelism, p.KeyLength)
+
+	b64Salt := base64.RawStdEncoding.EncodeToString(salt)
+	b64Hash := base64.RawStdEncoding.EncodeToString(hash)
+
+	encodedHash = fmt.Sprintf("$argon2id$v=%d$m=%d,t=%d,p=%d$%s$%s", argon2.Version, p.Memory, p.Iterations, p.Parallelism, b64Salt, b64Hash)
+
+	return encodedHash, nil
+}
+
+func generateRandomBytes(n uint32) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+var (
+	ErrHashInvalid             = errors.New("hash is weird")
+	ErrHashVersionIncompatible = errors.New("hash version is not good")
+)
+
+func verifyPassword(password, encodedHash string) (match bool, err error) {
+	p, salt, hash, err := decodeHash(encodedHash)
+	if err != nil {
+		return false, err
+	}
+
+	otherHash := argon2.IDKey([]byte(password), salt, p.Iterations, p.Memory, p.Parallelism, p.KeyLength)
+
+	if subtle.ConstantTimeCompare(hash, otherHash) == 1 {
+		return true, nil
+	}
+
+	return false, nil
+
+}
+
+func decodeHash(encodedHash string) (p *Argon2Params, salt, hash []byte, err error) {
+	vals := strings.Split(encodedHash, "$")
+	if len(vals) != 6 {
+		return nil, nil, nil, ErrHashInvalid
+	}
+
+	var version int
+	_, err = fmt.Sscanf(vals[2], "v=%d", &version)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	if version != argon2.Version {
+		return nil, nil, nil, ErrHashVersionIncompatible
+	}
+
+	p = &Argon2Params{}
+	_, err = fmt.Sscanf(vals[3], "m=%d,t=%d,p=%d", &p.Memory, &p.Iterations, &p.Parallelism)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	salt, err = base64.RawStdEncoding.Strict().DecodeString(vals[4])
+
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.SaltLength = uint32(len(salt))
+
+	hash, err = base64.RawStdEncoding.Strict().DecodeString(vals[5])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	p.KeyLength = uint32(len(hash))
+
+	return p, salt, hash, nil
 }
