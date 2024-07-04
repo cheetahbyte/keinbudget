bd7744429ef8f3cf5a34eb3b82e67259272d4575
Leonhard Breuer <me@leobreuer.dev>
2024-07-04 13:46:12 +0200

    chore(backend/main.go): remove unnessecary stuff

--- server/main.go
+++ server/main.go
@@ -6,32 +6,18 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func handler(c *fiber.Ctx) error {
-	result := c.Query("foo")
-	println(result)
-	c.SendString("You said: " + result)
-	return nil
-}
-
 func main() {
 	app := fiber.New()
 
 	database.SetupDatabase()
 	defer database.DB.Close()
 
-	// create handlers
 	userHandler := handlers.UserHandler{DB: database.DB}
 	userGroup := app.Group("/users")
 	{
 		userGroup.Post("/", userHandler.NewUser)
 		userGroup.Post("/login", userHandler.LoginUser)
 	}
-
-	// mount routes
-
-	app.Get("/", handler)
-
-	// start app
 
 	app.Listen(":3000")
 }
