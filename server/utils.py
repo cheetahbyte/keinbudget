f61fc92d8c2fe1d211299a8d92a42f51e8e8c296
Leonhard Breuer <me@leobreuer.dev>
2024-07-04 08:30:48 +0200

    cleanup old python files

--- server/utils.py
+++
@@ -1,6 +0,0 @@
-from database import ExternalAccount, Account
-
-
-async def fetch_account(account_id: str):
-    account = await Account.filter(id=account_id).first() or await ExternalAccount.filter(id=account_id).first()
-    return account
