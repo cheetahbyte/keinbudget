6c35e50c0d94715d2f3e04faa96f3cb1c553c249
Leonhard Breuer <me@leobreuer.dev>
2024-07-04 22:01:58 +0200

    feat(middleware/session): add session middleware

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/cheetahybte/keinbudget-backend/database"
 	"github.com/cheetahybte/keinbudget-backend/handlers"
+	"github.com/cheetahybte/keinbudget-backend/middleware/sessions"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,12 +13,28 @@ func main() {
 	database.SetupDatabase()
 	defer database.DB.Close()
 
+	sessionMiddlewareConfig := sessions.Config{
+		DB: database.DB,
+	}
+
+	app.Use(sessions.New(sessionMiddlewareConfig))
+
 	userHandler := handlers.UserHandler{DB: database.DB}
 	userGroup := app.Group("/users")
 	{
 		userGroup.Post("/", userHandler.NewUser)
 		userGroup.Post("/login", userHandler.LoginUser)
 	}
+	app.Get("/", func(c *fiber.Ctx) error {
+		var user handlers.User
+		user, ok := c.Locals("user").(handlers.User)
+
+		if !ok {
+			return c.Status(fiber.StatusInternalServerError).SendString("failed to retrieve user from session middleware")
+		}
+
+		return c.SendString(user.Username)
+	})
 
 	app.Listen(":3000")
 }
