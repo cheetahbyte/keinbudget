a652c74f6bd5676c1d44251fda9c3783b0d1a99b
Leonhard Breuer <me@leobreuer.dev>
2024-07-05 11:11:02 +0200

    feat(database): add table for transactions

--- server/database/setup.go
+++ server/database/setup.go
@@ -41,6 +41,16 @@ create table if not exists external_accounts(
 	created_at datetime default current_timestamp,
 	foreign key(user_id) references users(id) on delete cascade
 );
+
+create table if not exists transactions(
+	id uuid primary key,
+	user_id uuid not null,
+	fr_acc uuid not null,
+	to_acc uuid not null,
+	message text,
+	balance decimal(20,2) not null,
+	foreign key(user_id) references users(id) on delete cascade
+);
 `
 
 func SetupDatabase() {
