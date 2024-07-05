7370c47378625c8427bae289e500f124e85d0058
Leonhard Breuer <me@leobreuer.dev>
2024-07-05 09:51:31 +0200

    feat(database): add accounts table

--- server/database/setup.go
+++ server/database/setup.go
@@ -13,14 +13,23 @@ var DB *sqlx.DB
 var schema = `
 create table if not exists users(
 	id uuid primary key,
-	username text unique,
-	password text,
+	username text unique not null,
+	password text not null,
 	created_at datetime default current_timestamp
 );
 
 create table if not exists sessions(
 	id uuid primary key,
-	user_id uuid,
+	user_id uuid not null,
+	created_at datetime default current_timestamp,
+	foreign key(user_id) references users(id) on delete cascade
+);
+
+create table if not exists accounts(
+	id uuid primary key,
+	user_id uuid not null,
+	name text not null,
+	balance decimal(20, 2) not null,
 	created_at datetime default current_timestamp,
 	foreign key(user_id) references users(id) on delete cascade
 );
