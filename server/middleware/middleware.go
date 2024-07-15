01dd28cfc304adf323b7311d71f02896407e79e7
Leo Breuer <contact@cheetahbyte.dev>
2024-07-15 07:02:53 +0200

    feat: add middleware

---
+++ server/middleware/middleware.go
@@ -0,0 +1,17 @@
+package middleware
+
+import "net/http"
+
+type Middleware func(http.Handler) http.Handler
+
+func Chain(middlewares ...Middleware) Middleware {
+	return func(final http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var current http.Handler = final
+			for i := len(middlewares) - 1; i >= 0; i-- {
+				current = middlewares[i](current)
+			}
+			current.ServeHTTP(w, r)
+		})
+	}
+}
