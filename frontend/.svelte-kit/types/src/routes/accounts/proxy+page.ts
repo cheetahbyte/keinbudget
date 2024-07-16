262dfbac59f502e640145da1a6a5df6313942076
Leonhard Breuer <me@leobreuer.dev>
2024-07-16 17:15:27 +0200

    fix gitignore

--- frontend/.svelte-kit/types/src/routes/accounts/proxy+page.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-/** */
-export async function load() {
-    const req = await fetch("http://localhost:3000/accounts", {credentials: "include"})
-    const resp = await req.json()
-	return {
-		accounts: resp
-	};
-}
