25c7b0f8d754ef75e567e474779ce8fe3402f961
Leonhard Breuer <me@leobreuer.dev>
2024-07-18 11:52:46 +0200

    feat: delete old files.

--- frontend/src/routes/accounts/+page.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export async function load() {
-    const req = await fetch("http://localhost:3000/accounts", {credentials: "include"})
-    const resp = await req.json()
-	return {
-		accounts: resp
-	};
-}
