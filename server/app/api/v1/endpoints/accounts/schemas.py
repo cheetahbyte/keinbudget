5998b9581bf8c562d02f04e9cb8aee5e6ca86c90
Leonhard Breuer <git@leonhardbreuer.de>
2025-04-30 10:29:11 +0200

    fix: change type of start_balance field to float instead of Decimal

--- server/app/api/v1/endpoints/accounts/schemas.py
+++ server/app/api/v1/endpoints/accounts/schemas.py
@@ -8,4 +8,5 @@ AccountSchema = pydantic_model_creator(Account, name="Account")
 
 class CreateAccountSchema(BaseModel):
     name: str
-    start_balance: Annotated[Decimal, Field(strict=True, allow_inf_nan=True, max_digits=10, decimal_places=2)]
+    start_balance: float
+    
