299466be56ada6ec55e6e505583c9654e02ebf47
Leonhard Breuer <git@leonhardbreuer.de>
2025-05-04 16:34:47 +0200

    refactor: cleanup imports

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,10 +1,4 @@
 import { Outlet } from "react-router";
-/*
-import { AccountsServiceProvider } from "./api/services/accounts.provider";
-import { FinanceServiceProvider } from "./api/services/finance.provider";
-import { AuthServiceProvider } from "./api/services/login.provider";
-import { TransactionServiceProvider } from "./api/services/transactions.provider";
-import { UserServiceProvider } from "./api/services/user.provider";*/
 import { ThemeProvider } from "./components/theme";
 import { UserServiceProvider } from "./api/services/user.provider";
 
