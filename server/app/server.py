c6a6eebad08f0fa16fc3fa96a2c58538fc40690b
Leonhard Breuer <git@leonhardbreuer.de>
2025-05-05 07:33:02 +0200

    fix: 404 not found when routes are typed directly

--- server/app/server.py
+++ server/app/server.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 from tortoise.contrib.fastapi import register_tortoise
@@ -28,9 +29,12 @@ app.include_router(api_router, prefix="/api/v1")
 
 if os.getenv("ENVIRONMENT") == "production":
     app.mount("/", StaticFiles(directory="/app/server/static", html=True), name="static")
-    
 
-
+@app.get("/{full_path:path}")
+async def spa_handler(full_path: str):
+    if not full_path.startswith("api/"):
+        return FileResponse("/app/server/static/index.html")
+    raise HTTPException(status_code=404, detail="Not found")
 
 
 if __name__ == "__main__":
