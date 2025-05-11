ce2e8023b8a36740b852c975db4258e235572a0e
Leonhard Breuer <git@leonhardbreuer.de>
2025-05-11 21:10:25 +0200

    fix: add check wether user signup over webinterface is disabled.

--- server/app/api/v1/endpoints/users/router.py
+++ server/app/api/v1/endpoints/users/router.py
@@ -4,17 +4,24 @@ from app.api.v1.endpoints.users import crud
 from app.database.models import User
 from app.core.auth import get_current_user
 from uuid import UUID
+import os
 
 router = APIRouter()
 
+
 @router.post("/", response_model=UserSchema)
 async def create(user: UserCreateSchema):
-    user_obj = await crud.create_user(user.dict())
-    return user_obj
+    if os.getenv("USER_SIGNUP") == "true":
+        user_obj = await crud.create_user(user.dict())
+        return user_obj
+    else:
+        raise HTTPException(status_code=403, detail="User signup is disabled")
+
 
 @router.get("/me", response_model=UserSchema)
 async def get_my_user(user: User = Depends(get_current_user)):
     return user
+
 
 @router.get("/{user_id}", response_model=UserSchema)
 async def get_user(user_id: UUID):
